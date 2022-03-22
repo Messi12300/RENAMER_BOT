6b88b8eefc9fcd3f3b4c00595d0d1f23b941f424
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2022-03-22 15:20:19 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -49,6 +49,6 @@ async def refunc(client,message):
         		else:
         			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Documents",callback_data = "doc") ]])
         		# dont chenge this message.reply_text 
-        		await message.reply_text(f"```{out_filename}```MC",
+        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- ```{out_filename}```",
         		reply_to_message_id=mg_id,reply_markup = markup)
         		
